4c56c7559665f722e28bee4676667832f5d3874c
Carlos Enrique Tule Uscanga <carlosenrique.tule.contractor@bbva.com>
2023-01-18 19:25:22 -0600

    component commit

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 import CategoryCard from './Card/CategoryCard'
 import CharacterForm from './CharacterForm/CharacterForm'
 import Spinner from './Spinner/Spinner'
-import Tabledex from './Tabledex/TableDex'
+import Tabledex from './Tabledex/Tabledex'
 
 export { CategoryCard, CharacterForm, Spinner, Tabledex }
